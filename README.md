36194a054146c53c68c267b3e689dd0fb0861903
Seyona C <165888173+Seydabomb@users.noreply.github.com>
2024-09-09 10:10:52 -0600

    Completed section 3
    Completed section 3

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ Sections 3-8
 
 ## Section 3
 
-content
+Interactive Web Demo App that defines Components, JSX, Props, and State
+_ Outputting dynamic content
+_ Using the "children" prop
+_ Adding your own "on" props like "onSelect"
+_ use the "useState()" hook from React to update the UI
 
 ## Section 4
 
--- Section 3/src/App.jsx
+++ Section 3/src/App.jsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
+
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
+import { EXAMPLES } from "./data";
 
 function App() {
+	// React hook which must be used within react component functions
+	const [selectedTopic, setSelectedTopic] = useState();
+
 	function handleSelect(selectedButton) {
 		// selectedButton => 'components', 'jsx', 'props', 'state'
-		tabContent = selectedButton;
-		console.log(tabContent);
+		setSelectedTopic(selectedButton);
+		// console.log(selectedTopic);
 	}
 
+	console.log("APP COMPONENT EXECUTING");
+
+	// If we don't have a selected topic, it's true. Then render that paragraph for tabContent.
+	let tabContent = <p>Please select a topic.</p>;
+	// If we have a selected topic, it's true. Override the tabContent so it renders that div.
+	if (selectedTopic) {
+		tabContent = (
+			<div id="tab-content">
+				<h3>{EXAMPLES[selectedTopic].title}</h3>
+				<p>{EXAMPLES[selectedTopic].description}</p>
+				<pre>
+					<code>{EXAMPLES[selectedTopic].code}</code>
+				</pre>
+			</div>
+		);
+	}
 	return (
 		<div>
 			<Header></Header>
@@ -17,24 +39,39 @@ function App() {
 				<section id="core-concepts">
 					<h2>Core Concepts</h2>
 					<ul>
-						<CoreConcept
-							title={CORE_CONCEPTS[0].title}
-							description={CORE_CONCEPTS[0].description}
-							image={CORE_CONCEPTS[0].image}
-						></CoreConcept>
-						{/* spread operator adds all the key value pairs to the properties */}
-						<CoreConcept {...CORE_CONCEPTS[1]}></CoreConcept>
-						<CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
-						<CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
+						{/* Third Method outputs everything below in a dynamic way */}
+						{CORE_CONCEPTS.map((coreConceptItem) => (
+							<CoreConcept key={coreConceptItem.title} {...coreConceptItem} />
+						))}
 					</ul>
 				</section>
 				<section id="examples">
 					<h2>Examples</h2>
 					<menu>
-						<TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
-						<TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-						<TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-						<TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+						<TabButton
+							isSelected={selectedTopic === "components"}
+							onSelect={() => handleSelect("components")}
+						>
+							Components
+						</TabButton>
+						<TabButton
+							isSelected={selectedTopic === "jsx"}
+							onSelect={() => handleSelect("jsx")}
+						>
+							JSX
+						</TabButton>
+						<TabButton
+							isSelected={selectedTopic === "props"}
+							onSelect={() => handleSelect("props")}
+						>
+							Props
+						</TabButton>
+						<TabButton
+							isSelected={selectedTopic === "state"}
+							onSelect={() => handleSelect("state")}
+						>
+							State
+						</TabButton>
 					</menu>
 					{tabContent}
 				</section>
