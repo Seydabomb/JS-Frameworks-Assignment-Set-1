d0cd67f8e861ad0405c7f23c175d29dd5a2f3cb0
Seyona C <165888173+Seydabomb@users.noreply.github.com>
2024-09-11 12:59:30 -0600

    Timer issues #138
    Timer issues #138

--- Section 8/src/components/Player.jsx
+++ Section 8/src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+	// playerName is the value whe are getting from useRef()
+	const playerName = useRef();
+
+	const [enteredPlayerName, setEnteredPlayerName] = useState("");
+
+	function handleClick() {
+		setEnteredPlayerName(playerName.current.value);
+		playerName.current.value = "";
+	}
+
+	return (
+		<section id="player">
+			<h2>Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}</h2>
+			<p>
+				<input ref={playerName} type="text" />
+				<button onClick={handleClick}>Set Name</button>
+			</p>
+		</section>
+	);
 }
